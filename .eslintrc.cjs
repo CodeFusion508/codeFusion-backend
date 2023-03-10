d99e198cd79bd84cef900a2cfb4e673240373c60
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-10 15:11:02 -0600

    chore: update eslintrc and lint changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,24 +10,37 @@ module.exports = {
         "sourceType"  : "commonjs"
     },
     "rules": {
-        "key-spacing": [
+        "arrow-body-style": [
             "error",
-            {
-                "align": {
-                    "afterColon"  : true,
-                    "beforeColon" : true,
-                    "on"          : "colon"
-                }
-            }
+            "as-needed",
+            { "requireReturnForObjectLiteral": true }
         ],
+        "key-spacing": ["error", {
+            "align": {
+                "afterColon"  : true,
+                "beforeColon" : true,
+                "on"          : "colon"
+            }
+        }],
         "no-console"           : "warn",
         "no-debugger"          : "error",
         "no-dupe-keys"         : "error",
         "no-duplicate-imports" : "error",
+        "no-empty"             : "error",
         "no-trailing-spaces"   : "error",
         "no-unused-vars"       : "warn",
         "no-var"               : "error",
-        "quotes"               : [
+        "object-curly-newline" : ["error", {
+            "ExportDeclaration" : { "consistent": true, "minProperties": 3 },
+            "ImportDeclaration" : { "consistent": true, "minProperties": 3 },
+            "ObjectExpression"  : { "consistent": true, "minProperties": 3 },
+            "ObjectPattern"     : { "consistent": true, "minProperties": 3 }
+        }],
+        "prefer-destructuring": ["error", {
+            "array"  : true,
+            "object" : true
+        }],
+        "quotes": [
             "error",
             "double",
             {
@@ -35,11 +48,8 @@ module.exports = {
                 "avoidEscape"           : true
             }
         ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "sort-keys": [
+        "semi"      : ["error", "always"],
+        "sort-keys" : [
             "error",
             "asc",
             { "caseSensitive": true }
--- src/modules/users/users.ctrl.js
+++ src/modules/users/users.ctrl.js
@@ -1,14 +1,16 @@
 module.exports = (deps) =>
   Object
     .entries(module.exports)
-    .reduce((acc, [name, method]) => ({
-      ...acc,
-      [name]: method.bind(null, Object.assign({}, module.exports, deps))
-    }), {});
+    .reduce((acc, [name, method]) => {
+      return {
+        ...acc,
+        [name]: method.bind(null, Object.assign({}, module.exports, deps))
+      };
+    }, {});
 
 
 const createUser = async ({ services }, data) => {
-  const query = `CREATE (u: User {uuid: "${data.uuid}", totalExp: "0", weeklyExp: "0", email: "${data.email}", userName: "${data.userName}"})`;
+  const query = `CREATE (u: Student {uuid: "${data.uuid}", totalExp: "0", weeklyExp: "0", email: "${data.email}", userName: "${data.userName}"})`;
 
   return await services.neo4j.session.run(query);
 };
