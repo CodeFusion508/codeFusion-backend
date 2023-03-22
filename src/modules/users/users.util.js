2e6792d7440001b81d0d25beb18bc0c4421b8e45
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-22 17:58:44 -0600

    perf: made error handling simpler

--- src/modules/users/users.util.js
+++ src/modules/users/users.util.js
@@ -11,32 +11,12 @@ const endpointResponse = (res, next) => (promise) => promise.then((response) => 
 }).catch(({err, message}) => {
     let error;
 
-    switch (err) {
-        case 400:
-            error = new Error(message);
-            error.statusCode = 400;
-
-            break;
-        case 401:
-            error = new Error(message);
-            error.statusCode = 401;
-
-            break;
-        case 403:
-            error = new Error(message);
-            error.statusCode = 403;
-
-            break;
-        case 404:
-            error = new Error(message);
-            error.statusCode = 404;
-
-            break;
-        default:
-            error = new Error("An unexpected error occurred while processing your request. Please try again later, or contact us at our github.");
-            error.statusCode = 500;
-
-            break;
+    if (err) {
+        error = new Error(message);
+        error.statusCode = err;
+    } else {
+        error = new Error("An unexpected error occurred while processing your request. Please try again later, or contact us at our github.");
+        error.statusCode = 500;
     }
 
     next(error);
