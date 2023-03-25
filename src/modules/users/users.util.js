a8fa9b5d07f1420a4aa756bd8a901594d2092335
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-24 22:21:05 -0600

    chore: lint changes

--- src/modules/users/users.util.js
+++ src/modules/users/users.util.js
@@ -9,6 +9,7 @@ const endpointResponse = (res, next) => (promise) => promise.then((response) => 
     res.send(response);
 }).catch(({ err, message }) => {
     let error;
+
     if (err) {
         error = new Error(message);
         error.statusCode = err;
@@ -16,8 +17,11 @@ const endpointResponse = (res, next) => (promise) => promise.then((response) => 
         error = new Error("An unexpected error occurred while processing your request. Please try again later, or contact us at our github.");
         error.statusCode = 500;
     }
-    
+
     next(error);
 });
 
-module.exports = { endpointMethods, endpointResponse }
+module.exports = {
+    endpointMethods,
+    endpointResponse
+};
