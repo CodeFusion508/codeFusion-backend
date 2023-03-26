e9526b667a1e28af2631af21ea2b08a62dffdd15
Alfredo <61888719+Alfredo3232@users.noreply.github.com>
2023-03-26 17:48:23 +0000

    test: test joi validation
    * test: test joi validation
    
    Signed-off-by: Jethran Enrique Gómez San Gabriel <jegomez@veracruz.gob.mx>
    
    * test: add query tests
    
    * chore: lint changes
      style: tests inside one describe
    
    ---------
    
    Signed-off-by: Jethran Enrique Gómez San Gabriel <jegomez@veracruz.gob.mx>
    Co-authored-by: Jethran Enrique Gómez San Gabriel <jegomez@veracruz.gob.mx>

---
+++ src/modules/users/test/users.query.test.js
@@ -0,0 +1,69 @@
+const {
+    createUserQuery,
+    deleteUserQuery,
+    findRegisteredUser,
+    findUserQuery,
+    logInQuery
+} = require("../users.query.js");
+
+describe("users query tests", () => {
+    it("createUserQuery", () => {
+        const body = {
+            email    : "testing10390@gmail.com",
+            password : "1234",
+            userName : "testing800",
+        };
+        const uuid = "1c12d3x-123d1232c13";
+
+        const query = createUserQuery(uuid, body);
+
+        expect(query).toContain("CREATE (u: Student:User");
+        expect(query).toContain(`uuid: "${uuid}",`);
+        expect(query).toContain(`"${body.email}"`);
+        expect(query).toContain(`"${body.password}"`);
+        expect(query).toContain(`"${body.userName}"`);
+    });
+
+    it("deleteUserQuery", () => {
+        const params = {
+            uuid: "1c12d3x-123d1232c13",
+        };
+
+        const query = deleteUserQuery(params);
+
+        expect(query).toContain(`MATCH (u: User {uuid: "${params.uuid}"})`);
+        expect(query).toContain(`SET u:softDeleted;`);
+    });
+
+    it("findRegisteredUser", () => {
+        const body = {
+            email: "JohnnyAppleSeed@mail.com"
+        };
+
+        const query = findRegisteredUser(body);
+
+        expect(query).toContain(`MATCH (u: Student {email: "${body.email}"}) RETURN u;`);
+    });
+
+    it("logInQuery", () => {
+        const body = {
+            email: "JohnnyAppleSeed@mail.com"
+        };
+
+        const query = logInQuery(body);
+
+        expect(query).toContain(`MATCH (u: Student {email: "${body.email}"})`);
+        expect(query).toContain(`WHERE NOT u:softDeleted`);
+    });
+
+    it("findUserQuery", () => {
+        const params = {
+            uuid: "1c12d3x-123d1232c13",
+        };
+
+        const query = findUserQuery(params);
+
+        expect(query).toContain(`MATCH (u: User {uuid: "${params.uuid}"})`);
+        expect(query).toContain(`WHERE NOT u:softDeleted`);
+    });
+});
