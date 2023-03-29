cd9efc13f4ab45f57b34c351a3f7a9d6adfd23bf
José Ricardo Cedeño García <68524118+Ember96@users.noreply.github.com>
2023-03-29 09:43:50 -0700

    feat: adding Lessons CRUD
    * feat: adding Lessons CRUD
    
    * feat: find one lesson endpoint
       feat: delete one lesson endpoint
    
    ---------
    
    Co-authored-by: Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>

---
+++ src/modules/lessons/lesson.joi.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+module.exports = {
+    getUUID: Joi.object({
+        uuid: Joi.required()
+    }),
+    createLESSON: Joi.object({
+        path  : Joi.string().required(),
+        title : Joi.string().required(),
+        desc  : Joi.string().required()
+    }),
+    getLESSONS: Joi.object({
+        //
+    }),
+    updateLESSON: Joi.object({
+        uuid     : Joi.string().required(),
+        path     : Joi.string().optional(),
+        title    : Joi.string().optional(),
+        desc     : Joi.string().optional(),
+        totalExp : Joi.number().optional()
+    })
+};
--- src/modules/users/users.clean.js
+++ src/modules/users/users.clean.js
@@ -13,7 +13,23 @@ const cleanRecord = (data) => {
     data.node = obj;
 };
 
+const cleanRecords = (data) => {
+    let arr = [];
+    let obj;
+
+    for (let i = 0; i < data.node.length; i++) {
+        obj = {
+            ...data.node[i]._fields[0]
+        };
+
+        arr.push(obj);
+    }
+
+    data.node = arr;
+};
+
 module.exports = {
     cleanNeo4j,
-    cleanRecord
+    cleanRecord,
+    cleanRecords
 };
