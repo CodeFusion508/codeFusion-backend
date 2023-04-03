c3a08bf0149014e64d22a127753f4596235f8ef4
3fili3 <fpl08999@gmail.com>
2023-04-03 15:03:09 -0500

    Updated Clean Records

--- src/modules/contentDays/contentDays.ctrl.js
+++ src/modules/contentDays/contentDays.ctrl.js
@@ -78,16 +78,14 @@ const deleteContentDays = async ({ services }, params) => {
 
 const getOneContentDayByUuid = async ({ services }, params) => {
 
-    console.log(params)
     const query = getContentDaysRelationsQuery(params)
     let data = await services.neo4j.session.run(query);
 
     if (data.records.length == 0) throw { err: 404, message: "This content days does not exist, please check if you have a valid uuid." };
 
     data = cleanNeo4j(data);
-    // cleanRecords(data);
 
-    return data;
+    return cleanRecords(data);
     
 }
 
--- src/utils/cleanData.js
+++ src/utils/cleanData.js
@@ -22,16 +22,35 @@ const cleanRecord = (data) => {
     data.node = obj;
 };
 
-const cleanRecords = (data) => {
+const cleanRecords = (data = { stats: {}, node: []}) => {
+
     let arr = [];
     let obj;
+    const indexByProperties = 0
+
+    return data.node.map(value => {
+
+        const newObjTemp = {}
+        const obj = value._fields[indexByProperties]
+        for (const key in obj.properties) {
+
+            newObjTemp[key] = obj.properties[key]
+            const properties = obj.properties[key];
+            if(properties.hasOwnProperty('low')) {
+                newObjTemp[key] = obj.properties[key]['low']
+            } 
+        }
+
+        return newObjTemp
+
+    })
 
     for (let i = 0; i < data.node.length; i++) {
         obj = {
             ...data.node[i]._fields[0]
         };
 
-        obj.identity = obj.identity.low;
+        // obj.identity = obj.identity.low;
         for (const objKey in obj.properties) {
             if (obj.properties[objKey].low === undefined) {
                 continue;
