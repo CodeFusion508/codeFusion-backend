b10eca2230c0efe30c8df153061f773fe1f456fa
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-04-07 15:58:18 -0600

    feat: update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -51,6 +51,7 @@ module.exports = {
         ],
         "semi"                    : ["error", "always"],
         "eqeqeq"                  : ["error", "smart"],
+        "no-else-return"          : "error",
         "jest/no-identical-title" : "error",
     }
 };
