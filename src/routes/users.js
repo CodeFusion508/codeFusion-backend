76cdc0741f590ab774e2f7917f46178ece7abed1
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-04-13 20:13:37 -0600

    chore: change func names and position

--- src/routes/users.js
+++ src/routes/users.js
@@ -16,20 +16,23 @@ module.exports = (deps) => {
     const endPoint = endpointMethods(deps);
 
     return Router()
+        // Student CRUD
         .post("/", endPoint(body, CREATE_USER, signUp))
         .post("/login", endPoint(body, LOGIN_USER, logIn))
-        .post("/create/rel", auth.verifyToken,  endPoint(body, CREATE_RELATION, createRel))
         .get("/:uuid", auth.verifyToken, endPoint(params, GET_UUID, getUser))
         .put("/", auth.verifyToken, endPoint(body, UPDATE_USER, updateUser))
         .delete("/:uuid", auth.verifyToken, endPoint(params, GET_UUID, deleteUser))
+        // Student Relationships
+        .post("/create/rel", auth.verifyToken,  endPoint(body, CREATE_RELATION, createRel))
         .delete("/delete/rel", auth.verifyToken, endPoint(body, DELETE_RELATION, deleteRel));
 };
 
 
-const getUser = ({ ctrls }) => ({ data }, res, next) => endpointResponse(res, next)(ctrls.usersCtrl.getUser(data));
 const signUp = ({ ctrls }) => ({ data }, res, next) => endpointResponse(res, next)(ctrls.usersCtrl.createUser(data));
+const logIn = ({ ctrls }) => ({ data }, res, next) => endpointResponse(res, next)(ctrls.usersCtrl.logIn(data));
+const getUser = ({ ctrls }) => ({ data }, res, next) => endpointResponse(res, next)(ctrls.usersCtrl.getUser(data));
 const updateUser = ({ ctrls }) => ({ data }, res, next) => endpointResponse(res, next)(ctrls.usersCtrl.updateUser(data));
 const deleteUser = ({ ctrls }) => ({ data }, res, next) => endpointResponse(res, next)(ctrls.usersCtrl.deleteUser(data));
-const logIn = ({ ctrls }) => ({ data }, res, next) => endpointResponse(res, next)(ctrls.usersCtrl.logIn(data));
+
 const createRel = ({ ctrls }) => ({ data }, res, next) => endpointResponse(res, next)(ctrls.usersCtrl.createRel(data));
 const deleteRel = ({ ctrls }) => ({ data }, res, next) => endpointResponse(res, next)(ctrls.usersCtrl.deleteRel(data));
