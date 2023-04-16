7ee4df7e7e52939a046f9d35049d06262d638866
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-04-15 19:16:21 -0600

    test: add tests for sprints.joi

--- src/modules/sprints/test/sprints.joi.test.js
+++ src/modules/sprints/test/sprints.joi.test.js
@@ -6,28 +6,69 @@ const {
 
 describe("sprints joi tests", () => {
   describe("create sprint joi", () => {
-    it("give error when password is null", () => {
+    it("give error when title is number", () => {
       const body = {
-        email    : "testing10390@gmail.com",
-        password : null,
-        userName : "testing800"
+        path  : "something",
+        title : 123,
+        desc  : "something",
+        label : "something"
       };
 
       const { error } = CREATE_SPRINT.validate(body);
 
-      expect(error.details[0].message).toBe('"password" must be a string');
+      expect(error.details[0].message).toBe('"title" must be a string');
     });
 
-    it("give error when email is null", () => {
+    it("give error when path is number", () => {
       const body = {
-        email    : null,
-        password : "1234",
-        userName : "testing800"
+        path  : 8403129,
+        title : "something",
+        desc  : "something",
+        label : "something"
       };
 
       const { error } = CREATE_SPRINT.validate(body);
 
-      expect(error.details[0].message).toBe('"email" must be a string');
+      expect(error.details[0].message).toBe('"path" must be a string');
+    });
+  });
+
+  describe("sprint joi uuid", () => {
+    it("give error when no UUID given", () => {
+      const params = {};
+
+      const { error } = GET_UUID.validate(params);
+
+      expect(error.details[0].message).toBe('"uuid" is required');
+    });
+  });
+
+  describe("update sprint joi", () => {
+    it("give error when no uuid given", () => {
+      const body = {
+        totalExp : 11,
+        title    : "some1",
+        desc     : "thing.1",
+        path     : "here/"
+      };
+
+      const { error } = UPDATE_SPRINT.validate(body);
+
+      expect(error.details[0].message).toBe('"uuid" is required');
+    });
+
+    it("give error when path is number", () => {
+      const body = {
+        uuid     : "d93k240d-d94k032i",
+        totalExp : 11,
+        title    : "some1",
+        desc     : "thing.1",
+        path     : 494321
+      };
+
+      const { error } = UPDATE_SPRINT.validate(body);
+
+      expect(error.details[0].message).toBe('"path" must be a string');
     });
   });
 
