865372e668bf2069b4ad983e6abaa7acba7739b9
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-05-01 15:06:37 -0600

    perf: remove app in deps

--- src/app.js
+++ src/app.js
@@ -28,8 +28,6 @@ module.exports = (deps) => {
     app.listen(PORT, () => {
         process.stdout.write("Running at -> " + `\x1b[3m\x1b[96mhttp://localhost:${PORT}\x1b[39m\x1b[23\x1b[0m` + "\n");
     });
-
-    return app;
 };
 
 /*
