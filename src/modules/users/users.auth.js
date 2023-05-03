afa36c0e24726086e0e3162517c212080f8528e5
Ember96 <gnomeaddict96@gmail.com>
2023-05-02 23:19:35 -0700

    corrected SEED import

--- src/modules/users/users.auth.js
+++ src/modules/users/users.auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { SEED } = require("./config.js");
+const { SEED } = require("../../config/config");
 
 const verifyToken = (req, res, next) => {
     const { authorization } = req.headers;
