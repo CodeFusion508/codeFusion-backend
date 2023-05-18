4c9876da186b4167d6e6d5dcebe26769ff57c378
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-05-17 21:13:53 -0600

    test: add new test and change tests

--- src/modules/google/test/google.joi.test.js
+++ src/modules/google/test/google.joi.test.js
@@ -19,21 +19,23 @@ describe("google joi tests", () => {
 
       expect(error.details[0].message).toBe('"email" must be a string');
     });
-  });
-  describe("google user login joi", () => {
-    it("give error when no idToken is empty", () => {
+
+    it("give error when idToken is a number", () => {
       const body = {
-        idToken: "",
+        email    : "JuanDoe@mail.com",
+        userName : "testing800",
+        idToken  : 918274108
       };
 
-      const { error } = LOGIN_G_USER.validate(body);
+      const { error } = CREATE_G_USER.validate(body);
 
-      expect(error.details[0].message).toBe('"idToken" is not allowed to be empty');
+      expect(error.details[0].message).toBe('"idToken" must be a string');
     });
+  });
 
+  describe("google user login joi", () => {
     it("give error when idToken is not provided", () => {
-      const body = {
-      };
+      const body = {};
 
       const { error } = LOGIN_G_USER.validate(body);
 
@@ -42,7 +44,7 @@ describe("google joi tests", () => {
 
     it("give error when idToken is not a string", () => {
       const body = {
-        idToken: number,
+        idToken: 489405,
       };
 
       const { error } = LOGIN_G_USER.validate(body);
@@ -50,6 +52,46 @@ describe("google joi tests", () => {
       expect(error.details[0].message).toBe('"idToken" must be a string');
     });
   });
+
+  describe("get all answers joi", () => {
+    it("give error when idToken is not provided", () => {
+      const body = {};
+
+      const { error } = GET_ALL_ANSWERS.validate(body);
+
+      expect(error.details[0].message).toBe('"sheet_id" is required');
+    });
+
+    it("give error when idToken is not a string", () => {
+      const body = {
+        sheet_id: 949494,
+      };
+
+      const { error } = GET_ALL_ANSWERS.validate(body);
+
+      expect(error.details[0].message).toBe('"sheet_id" must be a string');
+    });
+  });
+
+  // describe("get all answers joi", () => {
+  //   it("give error when idToken is not provided", () => {
+  //     const body = {};
+
+  //     const { error } = GET_ALL_ANSWERS.validate(body);
+
+  //     expect(error.details[0].message).toBe('"sheet_id" is required');
+  //   });
+
+  //   it("give error when idToken is not a string", () => {
+  //     const body = {
+  //       sheet_id: 949494,
+  //     };
+
+  //     const { error } = GET_ALL_ANSWERS.validate(body);
+
+  //     expect(error.details[0].message).toBe('"sheet_id" must be a string');
+  //   });
+  // });
 });
 
 
