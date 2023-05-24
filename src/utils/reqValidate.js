8c462fea5907847db35426cd872e37a1761a5a66
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-05-24 12:20:29 -0600

    fix: joi sends error and message properly

--- src/utils/reqValidate.js
+++ src/utils/reqValidate.js
@@ -8,7 +8,7 @@ module.exports = (reqData, joi) => (req, res, next) => {
 
     if (error) {
         error.statusCode = 400;
-        return next(error);
+        res.status(error.statusCode).send(error.message);
     }
 
     req.data = value;
