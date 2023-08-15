6e2429647178cfc591fcc44592256a8b086c7bac
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-08-15 14:39:41 -0700

    test: add joi tests for content

--- src/modules/content/test/content.joi.test.js
+++ src/modules/content/test/content.joi.test.js
@@ -1,6 +1,9 @@
 const {
   GET_UUID,
-  CREATE_CONTENT,
+  CREATE_PROBLEM,
+  CREATE_QUIZ,
+  CREATE_VIDEO,
+  CREATE_TEXT,
   UPDATE_CONTENT
 } = require("../content.joi.js");
 
@@ -15,39 +18,169 @@ describe("content joi tests", () => {
     });
   });
 
-  describe("joi create CONTENT", () => {
+  describe("joi create PROBLEM", () => {
     it("give back value when body is correct", () => {
       const body = {
-        label     : "d0,e",
-        path      : "/3d3d3/",
         exp       : 10,
         title     : "something 1",
         desc      : "something 1/.",
         dayUuid   : "2k3-d4l42-3d-l4d23",
         contentNo : 6,
         time      : 100,
+
+        element  : "H1",
+        content  : "String content",
+        language : "Javascript"
       };
 
-      const { value } = CREATE_CONTENT.validate(body);
+      const { value } = CREATE_PROBLEM.validate(body);
 
-      expect(value).toHaveProperty("label");
-      expect(value).toHaveProperty("path");
-      expect(value).toHaveProperty("dayUuid");
+      expect(value).toHaveProperty("desc");
+      expect(value).toHaveProperty("element");
+      expect(value).toHaveProperty("language");
     });
 
     it("give error when no dayUuid given", () => {
       const body = {
-        label     : "d0,e",
-        path      : "/3d3d3/",
         exp       : 10,
         title     : "something 1",
         desc      : "something 1/.",
-        dayUuid   : "",
+        dayUuid   : "2k3-d4l42-3d-l4d23",
         contentNo : 6,
-        time      : 100
+        time      : 100,
+
+        element  : "H1",
+        content  : "String content",
+        language : "Javascript"
       };
 
-      const { error } = CREATE_CONTENT.validate(body);
+      const { error } = CREATE_PROBLEM.validate(body);
+
+      expect(error.details[0].message).toBe('"dayUuid" is not allowed to be empty');
+    });
+  });
+
+  describe("joi create QUIZ", () => {
+    it("give back value when body is correct", () => {
+      const body = {
+        exp       : 10,
+        title     : "something 1",
+        desc      : "something 1/.",
+        dayUuid   : "2k3-d4l42-3d-l4d23",
+        contentNo : 6,
+        time      : 100,
+
+        element  : "H1",
+        content  : "String content",
+        language : "Javascript"
+      };
+
+      const { value } = CREATE_QUIZ.validate(body);
+
+      expect(value).toHaveProperty("desc");
+      expect(value).toHaveProperty("element");
+      expect(value).toHaveProperty("language");
+    });
+
+    it("give error when no dayUuid given", () => {
+      const body = {
+        exp       : 10,
+        title     : "something 1",
+        desc      : "something 1/.",
+        dayUuid   : "2k3-d4l42-3d-l4d23",
+        contentNo : 6,
+        time      : 100,
+
+        element  : "H1",
+        content  : "String content",
+        language : "Javascript"
+      };
+
+      const { error } = CREATE_QUIZ.validate(body);
+
+      expect(error.details[0].message).toBe('"dayUuid" is not allowed to be empty');
+    });
+  });
+
+  describe("joi create VIDEO", () => {
+    it("give back value when body is correct", () => {
+      const body = {
+        exp       : 10,
+        title     : "something 1",
+        desc      : "something 1/.",
+        dayUuid   : "2k3-d4l42-3d-l4d23",
+        contentNo : 6,
+        time      : 100,
+
+        element  : "H1",
+        content  : "String content",
+        language : "Javascript"
+      };
+
+      const { value } = CREATE_VIDEO.validate(body);
+
+      expect(value).toHaveProperty("desc");
+      expect(value).toHaveProperty("element");
+      expect(value).toHaveProperty("language");
+    });
+
+    it("give error when no dayUuid given", () => {
+      const body = {
+        exp       : 10,
+        title     : "something 1",
+        desc      : "something 1/.",
+        dayUuid   : "2k3-d4l42-3d-l4d23",
+        contentNo : 6,
+        time      : 100,
+
+        element  : "H1",
+        content  : "String content",
+        language : "Javascript"
+      };
+
+      const { error } = CREATE_VIDEO.validate(body);
+
+      expect(error.details[0].message).toBe('"dayUuid" is not allowed to be empty');
+    });
+  });
+
+  describe("joi create TEXT", () => {
+    it("give back value when body is correct", () => {
+      const body = {
+        exp       : 10,
+        title     : "something 1",
+        desc      : "something 1/.",
+        dayUuid   : "2k3-d4l42-3d-l4d23",
+        contentNo : 6,
+        time      : 100,
+
+        element  : "H1",
+        content  : "String content",
+        language : "Javascript"
+      };
+
+      const { value } = CREATE_TEXT.validate(body);
+
+      expect(value).toHaveProperty("desc");
+      expect(value).toHaveProperty("element");
+      expect(value).toHaveProperty("language");
+    });
+
+    it("give error when no dayUuid given", () => {
+      const body = {
+        exp       : 10,
+        title     : "something 1",
+        desc      : "something 1/.",
+        dayUuid   : "2k3-d4l42-3d-l4d23",
+        contentNo : 6,
+        time      : 100,
+
+        element  : "H1",
+        content  : "String content",
+        language : "Javascript"
+      };
+
+      const { error } = CREATE_TEXT.validate(body);
 
       expect(error.details[0].message).toBe('"dayUuid" is not allowed to be empty');
     });
