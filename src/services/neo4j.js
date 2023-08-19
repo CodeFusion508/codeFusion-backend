2623971efea85a5c3523cf89d32c705b42b79975
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-08-19 14:29:27 -0700

    feat: add a interval job to keep db alive

--- src/services/neo4j.js
+++ src/services/neo4j.js
@@ -15,6 +15,20 @@ module.exports = async () => {
         if (!await driver.verifyAuthentication()) throw ({ message: "Auth neo4j error" });
         const session = driver.session();
 
+        const executeKeepAliveQuery = () => {
+            session.run("MATCH (n) RETURN count(n) AS nodeCount")
+                .then(result => {
+                    process.stdout.write(`Keep alive query executed successfully. Node count: ${result.records[0].get("nodeCount")}\n`);
+                })
+                .catch(error => {
+                    process.stdout.write("Error executing keep alive query:", error, "\n");
+                });
+        };
+
+        // Schedule the keep alive query to run every 30 minutes
+        const keepAliveInterval = 30 * 60 * 1000; // 30 minutes in milliseconds
+        setInterval(executeKeepAliveQuery, keepAliveInterval);
+
         const neo4j = { driver, session };
 
         return neo4j;
