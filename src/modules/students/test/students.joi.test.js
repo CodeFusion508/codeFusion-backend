c39c3416dda30932c22e0c4182ce1970e24bd832
Francisco Luna <pancho404x@gmail.com>
2023-08-24 17:32:22 -0600

    Update students.joi.test.js

--- src/modules/students/test/students.joi.test.js
+++ src/modules/students/test/students.joi.test.js
@@ -185,7 +185,7 @@ describe("Students Joi Tests", () => {
 
     it("Give value when body is correct", () => {
       const body = {
-        token: "Token?"
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
       };
 
       const { value } = CONFIRM_ACCOUNT.validate(body);
