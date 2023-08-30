c27eebe089b8653090a5c252175ee0c1bb1f1ff0
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-08-30 15:29:26 -0700

    chore: move comments in helpers.js chore: running at is more visible test: add new int tests for other endpoints test: add checks in existing int tests

--- integration/helpers.js
+++ integration/helpers.js
@@ -1,29 +1,5 @@
 const request = require("./supertest.js");
 
-/*
-Currently gives issues running this probably due to JEST, supertest and how we use fs to write UUIDs. You can fix it by setting a timeout for 2 seconds,
-however that is unacceptable due to performance and we have to make dummy data lots of times.
-*/
-/*
-const fs = require("fs");
-const path = require("path");
-
-const keepTrackOfUuids = async (UUID) => {
-    try {
-        const filePath = path.join(__dirname, "mockData.json");
-        const jsonData = fs.readFileSync(filePath, "utf8");
-        const data = await JSON.parse(jsonData);
-
-        data.dummyNodes.push(UUID);
-
-        fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-
-        console.log(`UUID ${UUID} added to the array.`);
-    } catch (error) {
-        console.error("Error:", error);
-    }
-};
-*/
 
 const makeDummySprint = async ({ sprintNo, title, desc }) => {
     const reqData = {
@@ -51,3 +27,29 @@ module.exports = {
     makeDummySprint,
     bulkDeleteDummySprints
 };
+
+
+/*
+Currently gives issues running this probably due to JEST, supertest and how we use fs to write UUIDs. You can fix it by setting a timeout for 2 seconds,
+however that is unacceptable due to performance and we have to make dummy data lots of times.
+*/
+/*
+const fs = require("fs");
+const path = require("path");
+
+const keepTrackOfUuids = async (UUID) => {
+    try {
+        const filePath = path.join(__dirname, "mockData.json");
+        const jsonData = fs.readFileSync(filePath, "utf8");
+        const data = await JSON.parse(jsonData);
+
+        data.dummyNodes.push(UUID);
+
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+
+        console.log(`UUID ${UUID} added to the array.`);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+*/
--- integration/tests/sprints.int.test.js
+++ integration/tests/sprints.int.test.js
@@ -6,28 +6,18 @@ const {
 
 
 describe("Sprints Integration Tests", () => {
-    const path = "/sprints/";
-
-    beforeAll(async () => {
-        const dummyData = {
-            sprintNo : 1993,
-            title    : "The Backrooms",
-            desc     : "If you're not careful and you noclip out of reality in the wrong areas"
-        };
-        await makeDummySprint(dummyData);
-        await makeDummySprint(dummyData);
-        await makeDummySprint(dummyData);
-        await makeDummySprint(dummyData);
-    });
+    const path = "/sprints";
 
     describe("GET /", () => {
-        it("Should get sprint nodes", async () => {
+        it("Should get all sprint nodes", async () => {
             const { body } = await request
                 .get(path)
                 .expect(200);
 
-            expect(body).toHaveProperty("stats");
-            expect(body).toHaveProperty("node");
+            for (const key in body.stats) {
+                expect(body.stats[key]).toBe(0);
+            }
+            expect(body.node.length > 1).toBeTruthy();
         });
     });
 
@@ -40,14 +30,107 @@ describe("Sprints Integration Tests", () => {
             };
 
             const { body } = await request
-                .post("/sprints/")
+                .post(path)
                 .send(reqData)
                 .expect(200);
 
-            expect(body).toHaveProperty("stats");
+            expect(body.stats).toHaveProperty("nodesCreated", 1);
+            expect(body.stats).toHaveProperty("labelsAdded", 1);
+            expect(body.node).toHaveProperty("title", reqData.title);
+            expect(body.node).toHaveProperty("sprintNo", reqData.sprintNo);
+        });
+    });
+
+    describe("PUT /", () => {
+        let UUID;
+
+        beforeAll(async () => {
+            const dummyData = {
+                sprintNo : 1993,
+                title    : "The Backrooms",
+                desc     : "If you're not careful and you noclip out of reality in the wrong areas"
+            };
+
+            const body = await makeDummySprint(dummyData);
+            UUID = body.node.uuid;
+        });
+
+        it("Should update sprint node", async () => {
+            const reqData = {
+                uuid     : UUID,
+                sprintNo : 1994,
+                title    : "Test - Doom II",
+                desc     : "Hell on Earth!",
+                totalExp : 1994
+            };
+
+            const { body } = await request
+                .put(path)
+                .send(reqData)
+                .expect(200);
+
+            expect(body.stats).toHaveProperty("propertiesSet", 4);
+            expect(body.node).toHaveProperty("title", reqData.title);
+            expect(body.node).toHaveProperty("sprintNo", reqData.sprintNo);
+            expect(body.node).toHaveProperty("totalExp", reqData.totalExp);
+        });
+    });
+
+    describe("GET /:uuid/info", () => {
+        let UUID;
+        let dummyData;
+
+        beforeAll(async () => {
+            dummyData = {
+                sprintNo : 1993,
+                title    : "The Backrooms",
+                desc     : "If you're not careful and you noclip out of reality in the wrong areas"
+            };
+
+            const body = await makeDummySprint(dummyData);
+            UUID = body.node.uuid;
+        });
+
+        it("Should get sprint node", async () => {
+            const { body } = await request
+                .get(path + `/${UUID}/info`)
+                .expect(200);
+
+
+            for (const key in body.stats) {
+                expect(body.stats[key]).toBe(0);
+            }
+            expect(body.node).toHaveProperty("uuid", UUID);
+            expect(body.node).toHaveProperty("title", "Test - " + dummyData.title);
+            expect(body.node).toHaveProperty("sprintNo", dummyData.sprintNo);
+        });
+    });
+
+    describe("DELETE /:uuid/node", () => {
+        let UUID;
+        let dummyData;
+
+        beforeAll(async () => {
+            dummyData = {
+                sprintNo : 1993,
+                title    : "The Backrooms",
+                desc     : "If you're not careful and you noclip out of reality in the wrong areas"
+            };
+
+            const body = await makeDummySprint(dummyData);
+            UUID = body.node.uuid;
+        });
+
+        it("Should get delete a sprint node", async () => {
+            const { body } = await request
+                .delete(path + `/${UUID}/node`)
+                .expect(200);
+
+            expect(body.stats).toHaveProperty("labelsAdded", 1);
             expect(body).toHaveProperty("node");
         });
     });
+
 
     afterAll(async () => {
         await bulkDeleteDummySprints();
