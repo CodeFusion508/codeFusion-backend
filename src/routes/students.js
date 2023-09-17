c23820ea3cd8045111cc870c65614d0a6b65de88
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-09-17 15:04:44 -0700

    fix: change students routes url names

--- src/routes/students.js
+++ src/routes/students.js
@@ -24,15 +24,15 @@ module.exports = (deps) => {
         // Student CRUD
         .post("/", endPoint(body, CREATE_STUDENT, signUp))
         .post("/login", endPoint(body, LOGIN_STUDENT, logIn))
-        .get("/:uuid", auth.verifyToken, endPoint(params, GET_UUID, getStudent))
+        .get("/node/:uuid", auth.verifyToken, endPoint(params, GET_UUID, getStudent))
         .put("/", auth.verifyToken, endPoint(body, UPDATE_STUDENT, updateStudent))
-        .delete("/:uuid", auth.verifyToken, endPoint(params, GET_UUID, deleteStudent))
+        .delete("/node/:uuid", auth.verifyToken, endPoint(params, GET_UUID, deleteStudent))
         // Student Relationships
-        .post("/create/rel", auth.verifyToken, endPoint(body, CREATE_RELATION, createRel))
+        .post("/rel", auth.verifyToken, endPoint(body, CREATE_RELATION, createRel))
         .delete("/rel", auth.verifyToken, endPoint(body, DELETE_RELATION, deleteRel))
         // Other Student Logic
-        .post("/confirm-account", endPoint(body, CREATE_STUDENT, WaitingForAccountConfirmation))
         .post("/recovery/account", endPoint(body, RECOVERY_ACCOUNT, recoveryAccount))
+        .post("/confirm-account", endPoint(body, CREATE_STUDENT, WaitingForAccountConfirmation))
         .get("/confirm-account-token/:token", endPoint(params, CONFIRM_ACCOUNT, confirmAccount));
 };
 
